ae1eb62aa5d27e5ce0075bdcc5e2cf3c0e09e63d
Adam Lang <Adam.Lang-1@hotmail.com>
2019-02-09 12:54:55 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Adam Lang on 2019-02-09.
+//  Copyright © 2019 Adam Lang. All rights reserved.
+//
+
+import Foundation
+
+class Item
+{
+    var title : String = ""
+    var done : Bool = false
+}
