22624d64e921ac989536beac42f1b2a5b2262ca4
Adam Lang <Adam.Lang-1@hotmail.com>
2019-02-09 12:01:40 +0100

    Data permitted using user defaults methods.

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -12,8 +12,16 @@ class ToDoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    if let items = defaults.array(forKey: "TodoListArray") as? [String]
+    {
+        itemArray = items
+    }
+        
         
     }
     
@@ -62,6 +70,8 @@ class ToDoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add item", style: .default) { (action) in
             //what will happen once the user clicks the add item button on our UIAlert
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.tableView.reloadData()
         }
